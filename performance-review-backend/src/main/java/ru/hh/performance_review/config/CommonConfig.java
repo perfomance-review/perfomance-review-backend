6e72b90edae7779481c9eee9ceb2dab671a0c234
igor-matyushenko <Z!x2c3v4b5>
2022-05-03 16:59:52 +0300

    Добавлена конфигурация запуска с параметрами БД из environment

--- performance-review-backend/src/main/java/ru/hh/performance_review/config/CommonConfig.java
+++ performance-review-backend/src/main/java/ru/hh/performance_review/config/CommonConfig.java
@@ -5,10 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ru.hh.nab.hibernate.MappingConfig;
 import ru.hh.nab.starter.NabCommonConfig;
-import ru.hh.performance_review.controller.ExampleController;
 
 @Configuration
-@Import({NabCommonConfig.class, ApplicationServiceConfig.class, LogbackConfigurator.class, ExampleController.class })
+@Import({NabCommonConfig.class, ApplicationServiceConfig.class, LogbackConfigurator.class})
 public class CommonConfig {
 
     @Bean
